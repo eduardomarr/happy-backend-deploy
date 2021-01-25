1c714ca854fc0546195383e28146e98304a43b22
Eduardo Marques <dado.nep@gmail.com>
2021-01-25 20:07:55 -0300

    backend-deploy

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -2,18 +2,19 @@ import { ErrorRequestHandler } from 'express';
 import { ValidationError } from 'yup';
 
 interface ValidationErrors {
-  [key: string]: string[]
+  [key: string]: string[];
 }
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
-
   if (error instanceof ValidationError) {
     let errors: ValidationErrors = {}
-
-    error.inner.forEach(err => {
-      errors[err.path] = err.errors;
+    
+  error.inner.forEach(err => {
+      if (err.path) {
+        errors[err.path] = err.errors
+      }
     });
-
+  
     return res.status(400).json({ message: "Validation fails", errors })
   }
 
